96bab21cd382fe450634be77c637741d92e9e38f
Gerald <102348985+geraldngjx@users.noreply.github.com>
2022-04-22 22:33:46 +0800

    initial commit

---
+++ Javascript-HTML-CSS/Mongoose Basics (with Express)/seeds.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const Product = require('./models/product');
+
+mongoose.connect('mongodb://localhost:27017/storeItems', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Mongo Connection Established.")
+    })
+    .catch(err => {
+        console.log("Connection Has Failed.")
+        console.log(err)
+    })
+
+const seedProducts = [
+    {
+        name: 'Fairy Eggplant',
+        price: 1.00,
+        category: 'vegetable'
+    },
+    {
+        name: 'Organic Goddess Melon',
+        price: 4.99,
+        category: 'fruit'
+    },
+    {
+        name: 'Organic Mini Seedless Watermelon',
+        price: 3.99,
+        category: 'fruit'
+    },
+    {
+        name: 'Organic Celery',
+        price: 1.50,
+        category: 'vegetable'
+    },
+    {
+        name: 'Chocolate Whole Milk',
+        price: 2.69,
+        category: 'dairy'
+    },
+]
+
+Product.insertMany(seedProducts)
+    .then(res => {
+        console.log(res)
+    })
+    .catch(e => {
+        console.log(e)
+    })
